2a933c1b089e6f33ebbe6adbd4b43d027515dca1
dendenjp <daimlerjp@gmail.com>
2022-09-15 23:37:21 -0500

    display num on input. stored in a variable

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
 'use strict';
-
-let display = document.querySelector('span');
 let firstOperand = 0;
 let secondOperand = 0;
-let currentOperand = 0;
+let pressedOperator = false;
+let pressedEqual = false;
+
+let displayScreen = document.querySelector('span');
+let backBtn = document.querySelector('.back');
+let clearBtn = document.querySelector('.clear');
+
+let operators = Array.from(document.querySelectorAll('.operator'));
 
 let numPad = Array.from(document.querySelectorAll('.numpad'));
-numPad.forEach((e) =>
-    e.addEventListener('click', function () {
-        if (e.textContent === 'C') display.textContent = '';
-        if (e.textContent === '←') {
-            display.textContent = display.textContent.slice(0, -1);
-        } else display.textContent = display.textContent + e.textContent;
-        firstOperand = Number(display.textContent);
-    })
-);
+
+backBtn.addEventListener('click', backSpace);
+clearBtn.addEventListener('click', clearScreen);
+numPad.forEach((e) => e.addEventListener('click', displayOnScreen));
+operators.forEach((e) => e.addEventListener('click', calculate));
+
+function displayOnScreen(e) {
+    displayScreen.innerText = +(displayScreen.innerText + e.target.id);
+    // firstOperand = +(displayScreen.innerText + e.target.id);
+    // displayScreen.innerText = firstOperand;
+}
+
+function calculate(e) {
+    // displayScreen.innerText = firstOperand;
+    firstOperand = +displayScreen.innerText;
+    console.log(typeof e.target.innerText);
+    if (e.target.innerText === '+') {
+        displayScreen.innertext = '';
+        console.log(firstOperand);
+    }
+}
+
+function backSpace() {
+    displayScreen.innerText = displayScreen.textContent.slice(0, -1);
+    // firstOperand = +displayScreen.innerText;
+}
+
+function clearScreen() {
+    displayScreen.innerText = '';
+    // firstOperand = +displayScreen.innerText;
+}
 
 function add(a, b) {
     return a + b;
@@ -41,10 +68,23 @@ function operate(a, operator, b) {
     if (operator === '*') return multiply(a, b);
     if (operator === '/') return divide(a, b);
 }
-
-console.log(operate(2, '+', 3));
-console.log(operate(2, '-', 3));
-console.log(operate(2, '*', 3));
-console.log(operate(2, '/', 3));
-console.log(operate(2.3, '*', 3.6));
-console.log(12 + 7 - 5 * 3);
+/* 
+function operate(a, operator, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+            break;
+        case '-':
+            return subtract(a, b);
+            break;
+        case '*':
+            return multiply(a, b);
+            break;
+        case '/':
+            return divide(a, b);
+            break;
+        default:
+            alert('Invalid');
+    }
+}
+ */
