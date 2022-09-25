febccc4e84f157aad917c8a0090d764b02f589d7
dendenjp <daimlerjp@gmail.com>
2022-09-25 01:43:36 -0500

    calc works pressing equals button twice is NaN

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let firstOperand = 0;
 let secondOperand = 0;
 let operator = '';
 let result = 0;
-console.log(operator);
+let isDisplayEmpty = true;
+let isOperator = false;
 
 const display = document.querySelector('.display-area');
 
@@ -11,37 +12,67 @@ const backBtn = document.querySelector('.back');
 const clearBtn = document.querySelector('.clear');
 const operators = Array.from(document.querySelectorAll('.operator'));
 const numpad = Array.from(document.querySelectorAll('.numpad'));
+const equalsBtn = document.querySelector('.equals');
 // const keypad = Array.from(document.querySelectorAll('.keypad'));
 
 backBtn.addEventListener('click', backSpace);
 clearBtn.addEventListener('click', clearScreen);
 numpad.forEach((e) => e.addEventListener('click', displayOnScreen));
 operators.forEach((e) => e.addEventListener('click', calculate));
+equalsBtn.addEventListener('click', equals);
 // keypad.forEach((e) => e.addEventListener('click', calculate));
 
 function displayOnScreen(e) {
-    display.innerText = '';
+    if (display.innerText == 0) {
+        display.innerText = '';
+    }
+
+    if (isDisplayEmpty == false) {
+        display.innerText = '';
+        isDisplayEmpty = true;
+    }
+
     display.innerText += e.target.innerText;
-    console.log(display.innerText);
 }
 
 function calculate(e) {
     if (operator == '') {
         operator = e.target.textContent;
-        console.log(operator);
+
         firstOperand = +display.innerText;
+        isDisplayEmpty = false;
     } else {
         secondOperand = +display.innerText;
-        result = operate(firstOperand, operator, secondOperand);
+        result =
+            Math.round(
+                operate(firstOperand, operator, secondOperand) * 100000
+            ) / 100000;
         displayResult();
         operator = e.target.textContent;
         firstOperand = result;
-        result = 0;
+        secondOperand = 0;
+
+        isDisplayEmpty = false;
     }
 }
 
+function equals(e) {
+    secondOperand = +display.innerText;
+    result =
+        Math.round(operate(firstOperand, operator, secondOperand) * 100000) /
+        100000;
+    displayResult();
+    firstOperand = 0;
+    operator = '';
+    isDisplayEmpty = false;
+}
+
 function displayResult() {
-    display.innerText = result;
+    if (result === undefined || result === NaN) {
+        display.innerText = 'Error. C to clear.';
+    } else if (result !== undefined || result !== NaN) {
+        display.innerText = result;
+    }
 }
 
 function backSpace(e) {
@@ -53,6 +84,8 @@ function clearScreen() {
     result = 0;
     firstOperand = 0;
     secondOperand = 0;
+    isDisplayEmpty = true;
+    operator = '';
 }
 
 function add(a, b) {
@@ -70,7 +103,7 @@ function multiply(a, b) {
 function divide(a, b) {
     if (b === 0) {
         console.log('Error');
-        return 'Error';
+        return (display.innerText = 'Division by ZERO');
     } else return a / b;
 }
 
@@ -89,6 +122,8 @@ function operate(a, operator, b) {
             return divide(a, b);
             break;
         default:
-            alert('Invalid');
+            // alert('Error');
+            display.innerText = 'ERROR. Press C to clear';
+            break;
     }
 }
